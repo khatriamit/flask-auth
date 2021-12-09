6cb94b1dca6c3181f6fa085677a04f0f661cc87f
pydevnepal <rajukhatri054@gmail.com>
2021-12-09 22:44:10 +0545

    create app configs

---
+++ auth/settings/config.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from entrypoints.routes import auth_routes
+from model.database import db
+
+
+def create_app(test_config=None):
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+    )
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY="dev", SQLALCHEMY_DATABASE_URI="sqlite:///./test.db"
+        )
+
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app = app
+    db.init_app(app)
+    app.register_blueprint(auth_routes)
+
+    return app
