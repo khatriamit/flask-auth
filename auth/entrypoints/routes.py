c491e04c4bf6d4e77fe98f2340fae19c1c696157
pydevnepal <rajukhatri054@gmail.com>
2021-12-10 06:03:33 +0545

    create rotue for add user

--- auth/entrypoints/routes.py
+++ auth/entrypoints/routes.py
@@ -1,11 +1,32 @@
-from flask import Blueprint
+import validators
+from flask import Blueprint, request, jsonify
+from model.database import db, User
+from werkzeug.security import check_password_hash, generate_password_hash
 
 auth_routes = Blueprint("auth_route", __name__, url_prefix="/api/v1/auth")
 
 
 @auth_routes.post("/user")
 def create_user():
-    return {"message": "user added successfully"}
+    full_name = request.json["full_name"]
+    username = request.json["username"]
+    email = request.json["email"]
+    password = request.json["password"]
+
+    if not validators.email(email):
+        return jsonify({"error": "email not valid"}, 400)
+
+    if User.objects.filter_by(email=email).first() is not None:
+        return jsonify({"error": "email already exists"}, 400)
+
+    if User.objects.filter_by(username=username).first() is not None:
+        return jsonify({"error": "username already exists"}, 400)
+
+    hash_pwd = generate_password_hash(password)
+    user = User(full_name=full_name, username=username, email=email, password=hash_pwd)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "user get successfully"}), 201
 
 
 @auth_routes.get("/user")
